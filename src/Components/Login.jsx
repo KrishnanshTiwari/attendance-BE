88f8f3c83d7fe504abdfdd0ffa9677bb34ea3381
Aditya kitukale <adityakitukale4599@gmail.com.com>
2024-07-16 20:02:58 +0530

    added commit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
   const [formState, setFormState] = useState({
     username: "",
     password: "",
   });
-  const [loggedIn, setLoggedIn] = useState(localStorage.getItem("token") !== null);
+  const [loggedIn, setLoggedIn] = useState(
+    localStorage.getItem("token") !== null
+  );
 
   const navigate = useNavigate();
 
   useEffect(() => {
     if (loggedIn) {
-      navigate('/webcam');
+      navigate("/webcam");
     }
   }, [loggedIn]);
 
@@ -28,52 +31,52 @@ const Login = () => {
     e.preventDefault();
     const { username, password } = formState;
     if (username === "admin" && password === "123") {
-      localStorage.setItem("token", "hbdhfbsgbjdskhiflasdadfkbeshfbesjbfusubkjahiakdnkndkbagsgduyw");
+      localStorage.setItem(
+        "token",
+        "hbdhfbsgbjdskhiflasdadfkbeshfbesjbfusubkjahiakdnkndkbagsgduyw"
+      );
       setLoggedIn(true);
     }
   };
 
   return (
-    <div>
-      <div className="container card border pt-5 pb-5" style={{ width: "350px" }}>
-        <h1 className="text-center">LogIn</h1>
-        <form onSubmit={submitForm}>
-          <div className="form-group mt-4">
-            <label htmlFor="user_login">Username</label>
-            <input
-              type="text"
-              name="username"
-              id="user_login"
-              className="form-control"
-              value={formState.username}
-              onChange={onChange}
-              placeholder="username"
-              required
-            />
-          </div>
-          <div className="form-group mt-4">
-            <label htmlFor="user_pass">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="user_pass"
-              className="form-control"
-              value={formState.password}
-              onChange={onChange}
-              placeholder="password"
-              required
-            />
-          </div>
-          <div className="form-group mt-4">
-            <input
-              type="submit"
-              className="btn btn-primary w-100"
-              value="Sign In"
-            />
-          </div>
-        </form>
+    <>
+      <div className="heading">BAC भारत</div>{" "}
+      <div className="container">
+        <div className="card">
+          <h1 className="heading-text">LogIn</h1>
+          <form onSubmit={submitForm}>
+            <div className="form-group">
+              <label htmlFor="user_login">Username</label>
+              <input
+                type="text"
+                name="username"
+                id="user_login"
+                value={formState.username}
+                onChange={onChange}
+                placeholder="username"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="user_pass">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="user_pass"
+                value={formState.password}
+                onChange={onChange}
+                placeholder="password"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input type="submit" className="submit" value="Sign In" />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/Components/WebcamDetection.jsx
+++ src/Components/WebcamDetection.jsx
@@ -1,6 +1,8 @@
-import React, { useRef, useEffect, useState } from 'react';
-import Webcam from 'react-webcam';
-import * as faceapi from 'face-api.js';
+import React, { useRef, useEffect, useState } from "react";
+import Webcam from "react-webcam";
+import * as faceapi from "face-api.js";
+import "./Webcam.css";
+import { useNavigate } from "react-router-dom";
 
 const WebcamDetection = () => {
   const webcamRef = useRef(null);
@@ -9,13 +11,21 @@ const WebcamDetection = () => {
   const [detected, setDetected] = useState(false);
   const [facesDetected, setFacesDetected] = useState(0);
   const [capturedImage, setCapturedImage] = useState(null);
+  const [faceRecognize, setFaceRecognized] = useState(false);
+  const [lastAttend, setLastAttend] = useState("Ashish Godiyal");
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     const loadModels = async () => {
       try {
         await Promise.all([
-          faceapi.nets.tinyFaceDetector.loadFromUri("/models/tiny_face_detector"),
-          faceapi.nets.faceLandmark68TinyNet.loadFromUri("/models/face_landmark_68_tiny"),
+          faceapi.nets.tinyFaceDetector.loadFromUri(
+            "/models/tiny_face_detector"
+          ),
+          faceapi.nets.faceLandmark68TinyNet.loadFromUri(
+            "/models/face_landmark_68_tiny"
+          ),
         ]);
         setIsLoading(false);
       } catch (error) {
@@ -28,28 +38,50 @@ const WebcamDetection = () => {
 
   const handleVideo = async () => {
     if (isLoading) return;
+    setIsLoading(true);
     if (webcamRef.current && webcamRef.current.video.readyState === 4) {
       const video = webcamRef.current.video;
       try {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions({ inputSize: 160, scoreThreshold: 0.5 }));
-        setBoundingBox(detections.map(d => d.box));
+        const detections = await faceapi.detectAllFaces(
+          video,
+          new faceapi.TinyFaceDetectorOptions({
+            inputSize: 160,
+            scoreThreshold: 0.5,
+          })
+        );
+        setBoundingBox(detections.map((d) => d.box));
         setDetected(detections.length > 0);
         setFacesDetected(detections.length);
         if (detections.length > 0) captureImage(detections[0].box);
+        setFaceRecognized(true);
+        setTimeout(() => {
+          setIsLoading(false);
+          setFaceRecognized(false);
+        }, 5000);
       } catch (error) {
-        console.error('Error detecting faces:', error);
+        console.error("Error detecting faces:", error);
       }
     }
   };
 
   const captureImage = (box) => {
     const video = webcamRef.current.video;
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
     canvas.width = box.width;
     canvas.height = box.height;
-    context.drawImage(video, box.x, box.y, box.width, box.height, 0, 0, box.width, box.height);
-    const faceImage = canvas.toDataURL('image/jpeg');
+    context.drawImage(
+      video,
+      box.x,
+      box.y,
+      box.width,
+      box.height,
+      0,
+      0,
+      box.width,
+      box.height
+    );
+    const faceImage = canvas.toDataURL("image/jpeg");
     setCapturedImage(faceImage);
   };
 
@@ -62,40 +94,55 @@ const WebcamDetection = () => {
     }
   }, [isLoading]);
 
+  const logout = () => {
+    console.log("logout");
+    const con = confirm("Are you want to logout?");
+    if (con) {
+      localStorage.removeItem("token");
+      navigate("/");
+    }
+  };
+
   return (
     <>
-
-      <div style={{ width: '100vw', height: '100%', overflow: 'hidden' }}>
+      <div className="header">
+        <span className="back-btn">BAC भारत</span>{" "}
+        <button className="logout-btn" onClick={logout}>
+          Logout <i className="fa-solid fa-right-from-bracket"></i>
+        </button>
+      </div>
+      <div className="webcam-container">
         <Webcam
           ref={webcamRef}
           forceScreenshotSourceSize
+          mirrored
           videoContainerStyle={{
-            position: 'relative',
-            width: '100vw',
-            height: '100vh',
-            overflow: 'hidden',
+            position: "relative",
+            width: "100vw",
+            height: "100%",
+            overflow: "hidden",
           }}
-          // videoStyle={{
-          //   transform: 'scale(0.2)',
-          //   transformOrigin: 'center',
-          //   width: '100%', 
-          //   height: 'auto', 
-          // }}
         />
         {boundingBox.map((box, index) => (
           <div
             key={index}
             style={{
-              border: '4px solid red',
-              position: 'absolute',
+              border: "4px solid #001f3f",
+              position: "absolute",
               top: box.top,
               left: box.left,
               width: box.width,
               height: box.height,
-              pointerEvents: 'none',
+              pointerEvents: "none",
             }}
           />
         ))}
+      </div>
+
+      <div className="info">
+        {faceRecognize
+          ? `"${lastAttend} your attendance was successful marked"`
+          : `"Look into the camera to mark your attendance automatically with face recognition."`}
       </div>
       {capturedImage && <img src={capturedImage} alt="Captured face" />}
     </>
@@ -103,4 +150,3 @@ const WebcamDetection = () => {
 };
 
 export default WebcamDetection;
-
